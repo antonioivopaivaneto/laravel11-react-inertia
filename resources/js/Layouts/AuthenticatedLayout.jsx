15077458ab1f152ab1b00c8189d3d396974687e4
junior <antonioivo.3@gmail.com>
2024-06-05 12:52:03 -0300

    menu vertical

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -10,62 +10,59 @@ export default function AuthenticatedLayout({ user, header, children }) {
     useState(false);
 
   return (
-    <div className="min-h-screen bg-gray-100">
-      <nav className="bg-white border-b border-gray-100">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="flex justify-between h-16">
-            <div className="flex">
-              <div className="shrink-0 flex items-center">
-                <Link href="/">
-                  <ApplicationLogo className="block h-9 w-auto fill-current text-gray-800" />
-                </Link>
-              </div>
-
-              <div className="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                <NavLink
-                  href={route("dashboard")}
-                  active={route().current("dashboard")}
-                >
-                  Dashboard
-                </NavLink>
-                <NavLink
-                  href={route("project.index")}
-                  active={route().current("project.index")}
-                >
-                  Projects
-                </NavLink>
-                <NavLink
-                  href={route("task.index")}
-                  active={route().current("task.index")}
-                >
-                 All Tasks
-                </NavLink>
-                <NavLink
-                  href={route("user.index")}
-                  active={route().current("user.index")}
-                >
-                  Users
-                </NavLink>
-                <NavLink
-                  href={route("task.myTasks")}
-                  active={route().current("task.myTasks")}
-                >
-                  My Tasks
-                </NavLink>
-              </div>
+    <div className="min-h-screen bg-gray-100 flex">
+      <nav className="bg-white border-r border-gray-100 w-64">
+        <div className="px-4 py-6">
+          <div className="flex flex-col items-center">
+            <div className="shrink-0 flex items-center mb-6">
+              <Link href="/">
+                <ApplicationLogo className="block h-9 w-auto fill-current text-gray-800" />
+              </Link>
             </div>
 
-            <div className="hidden sm:flex sm:items-center sm:ms-6">
-              <div className="ms-3 relative">
+            <div className="flex flex-col space-y-4 w-full">
+              <NavLink
+                href={route("dashboard")}
+                active={route().current("dashboard")}
+              >
+                Dashboard
+              </NavLink>
+              <NavLink
+                href={route("project.index")}
+                active={route().current("project.index")}
+              >
+                Projects
+              </NavLink>
+              <NavLink
+                href={route("task.index")}
+                active={route().current("task.index")}
+              >
+                All Tasks
+              </NavLink>
+              <NavLink
+                href={route("user.index")}
+                active={route().current("user.index")}
+              >
+                Users
+              </NavLink>
+              <NavLink
+                href={route("task.myTasks")}
+                active={route().current("task.myTasks")}
+              >
+                My Tasks
+              </NavLink>
+            </div>
+
+            <div className="mt-auto w-full">
+              <div className="mt-6">
                 <Dropdown>
                   <Dropdown.Trigger>
-                    <span className="inline-flex rounded-md">
+                    <span className="inline-flex rounded-md w-full">
                       <button
                         type="button"
-                        className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150"
+                        className="inline-flex items-center justify-between w-full px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150"
                       >
                         {user.name}
-
                         <svg
                           className="ms-2 -me-0.5 h-4 w-4"
                           xmlns="http://www.w3.org/2000/svg"
@@ -97,95 +94,21 @@ export default function AuthenticatedLayout({ user, header, children }) {
                 </Dropdown>
               </div>
             </div>
-
-            <div className="-me-2 flex items-center sm:hidden">
-              <button
-                onClick={() =>
-                  setShowingNavigationDropdown(
-                    (previousState) => !previousState
-                  )
-                }
-                className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
-              >
-                <svg
-                  className="h-6 w-6"
-                  stroke="currentColor"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    className={
-                      !showingNavigationDropdown ? "inline-flex" : "hidden"
-                    }
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M4 6h16M4 12h16M4 18h16"
-                  />
-                  <path
-                    className={
-                      showingNavigationDropdown ? "inline-flex" : "hidden"
-                    }
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                </svg>
-              </button>
-            </div>
-          </div>
-        </div>
-
-        <div
-          className={
-            (showingNavigationDropdown ? "block" : "hidden") + " sm:hidden"
-          }
-        >
-          <div className="pt-2 pb-3 space-y-1">
-            <ResponsiveNavLink
-              href={route("dashboard")}
-              active={route().current("dashboard")}
-            >
-              Dashboard
-            </ResponsiveNavLink>
-          </div>
-
-          <div className="pt-4 pb-1 border-t border-gray-200">
-            <div className="px-4">
-              <div className="font-medium text-base text-gray-800">
-                {user.name}
-              </div>
-              <div className="font-medium text-sm text-gray-500">
-                {user.email}
-              </div>
-            </div>
-
-            <div className="mt-3 space-y-1">
-              <ResponsiveNavLink href={route("profile.edit")}>
-                Profile
-              </ResponsiveNavLink>
-              <ResponsiveNavLink
-                method="post"
-                href={route("logout")}
-                as="button"
-              >
-                Log Out
-              </ResponsiveNavLink>
-            </div>
           </div>
         </div>
       </nav>
 
-      {header && (
-        <header className="bg-white shadow">
-          <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-            {header}
-          </div>
-        </header>
-      )}
+      <div className="flex-1">
+        {header && (
+          <header className="bg-white shadow">
+            <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+              {header}
+            </div>
+          </header>
+        )}
 
-      <main>{children}</main>
+        <main>{children}</main>
+      </div>
     </div>
   );
 }
